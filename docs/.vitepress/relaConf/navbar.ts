2b5c173c1175be3b5a2c8c5f3f91ef1476c6d133
kai.qiao <kai.qiao@shopee.com>
2023-11-16 12:02:41 +0800

    update eth2.0

--- docs/.vitepress/relaConf/navbar.ts
+++ docs/.vitepress/relaConf/navbar.ts
@@ -1,55 +1,70 @@
-export * from './navbar';
+export * from './navbar'
 
-import { DefaultTheme } from 'vitepress';
+import { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.NavItem[] = [
   {
     text: '首页',
-    link: '/' // 表示docs/index.md
+    link: '/', // 表示docs/index.md
+  },
+  {
+    text: '区块链技术',
+    items: [
+      {
+        text: 'ETH2.0与智能合约',
+        link: '/blockchain/ETH/', // 对应docs/blockchain/ETH/index.md
+      },
+      {
+        text: 'BTC与区块链',
+        link: '#', //
+      },
+    ],
   },
   {
     text: '前端开发',
     items: [
       {
         text: '数据结构与算法',
-        link: '/column/Algorithm/' // 对应docs/column/Algorithm下的idnex.md文件
+        link: '/column/Algorithm/', // 对应docs/column/Algorithm下的idnex.md文件
       },
       {
         text: 'JavaScript学习',
-        link: '/guide/javascript/' // 对应docs/column/Algorithm下的idnex.md文件
-      }
-    ]
+        link: '/guide/javascript/', // 对应docs/column/Algorithm下的idnex.md文件
+      },
+    ],
   },
   {
     text: '个人成长',
     items: [
       {
         text: '大江南北游记',
-        link: '/column/Travel/' // 表示docs/column/Travel/index.md
+        link: '/column/Travel/', // 表示docs/column/Travel/index.md
       },
       {
         text: '所思·所想',
-        link: '/column/Growing/' // 表示docs/column/Growing/index.md
+        link: '/column/Growing/', // 表示docs/column/Growing/index.md
       },
       { text: 'WebExt', link: 'guide/webExt/webExt' },
       {
         text: 'Bilibili',
         link: 'https://space.bilibili.com/211794005?spm_id_from=333.1007.0.0',
       },
-    ]
+    ],
   },
   {
     text: '关于我',
     items: [
-      { text: 'Github', link: 'https://github.com/joqk12345/master-fronted-tech' },
+      {
+        text: 'Github',
+        link: 'https://github.com/joqk12345/master-fronted-tech',
+      },
       {
         text: '掘金',
-        link: 'https://juejin.cn/user/3131845139247960/posts'
+        link: 'https://juejin.cn/user/3131845139247960/posts',
       },
       {
         text: '小红书',
-        link: 'https://www.xiaohongshu.com/user/profile/6336ea62000000001802a16e'
-      }
-    ]
-  }
-];
-
+        link: 'https://www.xiaohongshu.com/user/profile/6336ea62000000001802a16e',
+      },
+    ],
+  },
+]
--- docs/.vitepress/relaConf/sidebar.ts
+++ docs/.vitepress/relaConf/sidebar.ts
@@ -1,22 +1,33 @@
-export * from './sidebar';
-import { DefaultTheme } from 'vitepress';
+export * from './sidebar'
+import { DefaultTheme } from 'vitepress'
 
 export const sidebar: DefaultTheme.Sidebar = {
+  'blockchain/ETH/': [
+    {
+      text: '智能合约',
+      items: [
+        {
+          text: '智能合约介绍',
+          link: '/blockchain/ETH/001_eth2.0',
+        },
+      ],
+    },
+  ],
   // column/Algothm/表示对这个文件夹下的所有md文件做侧边栏配置
   'column/Algorithm/': [
-     // 第一部分
+    // 第一部分
     {
       text: '栈和队列',
       items: [
         {
           text: '栈-深拷贝和浅拷贝',
-          link: '/column/Algorithm/001_stack'
+          link: '/column/Algorithm/001_stack',
         },
         {
           text: '队列-事件循环',
-          link: '/column/Algorithm/002_queue'
-        }
-      ]
+          link: '/column/Algorithm/002_queue',
+        },
+      ],
     },
     // 第二部分
     {
@@ -24,44 +35,40 @@ export const sidebar: DefaultTheme.Sidebar = {
       items: [
         {
           text: '字典和集合-Set和Map',
-          link: '/column/Algorithm/003_dictionary'
+          link: '/column/Algorithm/003_dictionary',
         },
         {
           text: '树-深/广度优先遍历',
-          link: '/column/Algorithm/004_tree'
-        }
-      ]
-    }
+          link: '/column/Algorithm/004_tree',
+        },
+      ],
+    },
   ],
   'guide/javascript/': [
     {
-        text: 'Javascript',
-        items: [
-          { text: 'Promise', link: '/guide/javascript/001_basic'},
-        ],
-      },
+      text: 'Javascript',
+      items: [{ text: 'Promise', link: '/guide/javascript/001_basic' }],
+    },
   ],
-  'guide/':[
+  'guide/': [
     {
-      text: 'Vue3', items: [
-          {'text': 'Store', link: '/guide/vue3/001-pinia'},
-        ]
-      },
-      {
-        text: 'CSS',
-        items: [
-          // { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: 'Bootstrap', link: 'guide/bootstrap/bootstrap-beginer' },
-          { text: 'CSS3', link: 'guide/css/001_layout' },
-        ],
-      },
-      {
-        text: 'HTML',
-        items: [
-          // { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: 'HTML5', link: '/api-examples' },
-        ],
-      },
-    ]
-};
-
+      text: 'Vue3',
+      items: [{ text: 'Store', link: '/guide/vue3/001-pinia' }],
+    },
+    {
+      text: 'CSS',
+      items: [
+        // { text: 'Runtime API Examples', link: '/api-examples' },
+        { text: 'Bootstrap', link: 'guide/bootstrap/bootstrap-beginer' },
+        { text: 'CSS3', link: 'guide/css/001_layout' },
+      ],
+    },
+    {
+      text: 'HTML',
+      items: [
+        // { text: 'Runtime API Examples', link: '/api-examples' },
+        { text: 'HTML5', link: '/api-examples' },
+      ],
+    },
+  ],
+}
