253ac503ad29c0e4574ad8a9e5d9d25ac34a578b
kai.qiao <kai.qiao@shopee.com>
2024-01-31 21:23:46 +0800

    add 0208

--- docs/.vitepress/relaConf/sidebar.ts
+++ docs/.vitepress/relaConf/sidebar.ts
@@ -90,99 +90,108 @@ export const sidebar: DefaultTheme.Sidebar = {
       text: '2024-01',
       items: [
         {
-          text: '1109-任务启动',
+          text: '0109-任务启动',
           link: '/content/English/2024/20240109.md',
         },
         {
-          text: '1110-对高管的看法',
+          text: '0110-对高管的看法',
           link: '/content/English/2024/20240110.md',
         },
         {
-          text: '1111-数字重塑人生',
+          text: '0111-数字重塑人生',
           link: '/content/English/2024/20240111.md',
         },
         {
-          text: '1112-康威定律的应用',
+          text: '0112-康威定律的应用',
           link: '/content/English/2024/20240112.md',
         },
         {
-          text: '1113-能源、智能和灵活性如何影响经济',
+          text: '0113-能源、智能和灵活性如何影响经济',
           link: '/content/English/2024/20240113.md',
         },
         {
-          text: '1114-有效加速主义',
+          text: '0114-有效加速主义',
           link: '/content/English/2024/20240114.md',
         },
         {
-          text: '1115-数字化转型',
+          text: '0115-数字化转型',
           link: '/content/English/2024/20240115.md',
         },
         {
-          text: '1116-经济机器的最小结构',
+          text: '0116-经济机器的最小结构',
           link: '/content/English/2024/20240116.md',
         },
         {
-          text: '1117-经济增长的三个主要动力',
+          text: '0117-经济增长的三个主要动力',
           link: '/content/English/2024/20240117.md',
         },
         {
-          text: '1118-债务周期',
+          text: '0118-债务周期',
           link: '/content/English/2024/20240118.md',
         },
         {
-          text: '1119-现代经济体系',
+          text: '0119-现代经济体系',
           link: '/content/English/2024/20240119.md',
         },
         {
-          text: '1120-技术是通缩通的',
+          text: '0120-技术是通缩通的',
           link: '/content/English/2024/20240120.md',
         },
         {
-          text: '1121-软件吞噬世界',
+          text: '0121-软件吞噬世界',
           link: '/content/English/2024/20240121.md',
         },
         {
-          text: '1122-六大描述技术通缩的定律',
+          text: '0122-六大描述技术通缩的定律',
           link: '/content/English/2024/20240122.md',
         },
         {
-          text: '1123-技术的瓶颈',
+          text: '0123-技术的瓶颈',
           link: '/content/English/2024/20240123.md',
         },
         {
-          text: '1124-行业发展的拐点',
+          text: '0124-行业发展的拐点',
           link: '/content/English/2024/20240124.md',
         },
         {
-          text: '1125-GDP的弊端',
+          text: '0125-GDP的弊端',
           link: '/content/English/2024/20240125.md',
         },
         {
-          text: '1126-经济的价值的核心是什么',
+          text: '0126-经济的价值的核心是什么',
           link: '/content/English/2024/20240126.md',
         },
         {
-          text: '1127-过去的经济与现代的经济增长',
+          text: '0127-过去的经济与现代的经济增长',
           link: '/content/English/2024/20240127.md',
         },
         {
-          text: '1128-由消费经济到体验经济',
+          text: '0128-由消费经济到体验经济',
           link: '/content/English/2024/20240128.md',
         },
         {
-          text: '1129-经济如何增长',
+          text: '0129-经济如何增长',
           link: '/content/English/2024/20240129.md',
         },
         {
-          text: '1130-Metaverse-构造的数字世界',
+          text: '0130-Metaverse-构造的数字世界',
           link: '/content/English/2024/20240130.md',
         },
         {
-          text: '1131-富足-通用计算与通用智能（软件吞噬世界，人工智能吞噬软件！）',
+          text: '0131-富足-通用计算与通用智能（软件吞噬世界，人工智能吞噬软件！）',
           link: '/content/English/2024/20240131.md',
-        },
+        }
       ],
     },
+      {
+          text:'02',
+          items:[
+              {
+                  text: '0201--',
+                  link: '/content/English/2024/20240201.md',
+              }
+          ]
+      }
   ],
   'guide/javascript/': [
     {
